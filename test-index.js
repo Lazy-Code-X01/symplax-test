0dc70f39ac6746201b1382a60b6af05169ef98a5
Lazy-Code-X01 <lazycode686@gmail.com>
2026-03-24 11:02:14 +0100

    first commit

---
+++ test-index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Symplax Test: It Works! 🚀</h1><p>This is a simple Node.js server.</p>');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
