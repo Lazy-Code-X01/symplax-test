2cbca1356566c631fa0ec2e582f7805668e8c581
Adeyemi David Adejuwon <93537427+Lazy-Code-X01@users.noreply.github.com>
2026-07-17 21:13:50 +0100

    Update test-index.js

--- test-index.js
+++ test-index.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end(
-    '<h1>Symplax Test: It Works! 🚀</h1><p>This is a simple Node.js server, testing auto push</p>'
+    '<h1>Symplax Test: It Works! 🚀</h1><p>This is a simple Node.js server</p>'
   );
 });
 
